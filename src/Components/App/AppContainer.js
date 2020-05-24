f13fed686d50874b581cba8f7e98d95e26cc6a45
k012497 <k012497@gmail.com>
2020-05-25 01:20:17 +0900

    Add delete and check noti function

--- src/Components/App/AppContainer.js
+++ src/Components/App/AppContainer.js
@@ -3,9 +3,65 @@ import AppPresenter from "./AppPresenter";
 import Store from "store";
 
 class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this._deleteNotification = id => {
+      console.log(id);
+      this.setState(currentState => {
+        const newState = delete currentState.notifications[id];
+        return newState;
+      })
+    }
+
+    this._seeNotification = id => {
+      alert(this.state.notifications[id].text);
+
+      this.setState(currentState => {
+        return {
+          ...currentState,
+          notifications : {
+            ...currentState.notifications,
+            [id] : {
+              ...currentState.notifications[id],
+              seen : true
+            }
+          }
+        }
+      });
+    }
+
+    this.state = {
+      notifications:{
+        "1" : {
+          id : 1,
+          text : "blahblah 📌",
+          seen : false
+        },
+        "2" : {
+          id : 2,
+          text : "blahblah 👑",
+          seen : true
+        },
+        "3" : {
+          id : 3,
+          text : "blahblah 🐜",
+          seen : false
+        },
+        "4" : {
+          id : 4,
+          text : "blahblah 😎",
+          seen : false
+        }
+      },
+      deleteNotification : this._deleteNotification,
+      seeNotification : this._seeNotification
+    };
+  } // 클래스가 생성되었을 때 store가 value를 얻는다.
+
   render() {
     return (
-      <Store.Provider>
+      <Store.Provider value={this.state}>
         <AppPresenter />
       </Store.Provider>
     );
--- src/Components/Notification/NotificationPresenter.js
+++ src/Components/Notification/NotificationPresenter.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Flex, { FlexItem } from "styled-flex-component";
 import FontAwesome from "react-fontawesome";
+import Store from "store";
 
 const Notification = styled.div`
   background-color: white;
@@ -54,15 +55,24 @@ const Button = styled.button`
 const NotificationPresenter = ({ id, text, seen }) => (
   <Notification seen={seen}>
     <Flex alignCenter justifyBetween>
-      <Title>{text}</Title>
+      <Title>
+        {text}
+      </Title>
       <FlexItem>
         <Fragment>
-          <Button success seen={seen} onClick={() => {}}>
-            <FontAwesome name="check" />
-          </Button>
-          <Button danger seen={seen} onClick={() => {}}>
-            <FontAwesome name="times" />
-          </Button>
+          <Store.Consumer>
+            { store => (
+              <>
+                <Button success seen={seen} onClick={() => store.seeNotification(id)}>
+                  <FontAwesome name="check" />
+                </Button>
+                <Button danger seen={seen} onClick={() => store.deleteNotification(id)}>
+                  <FontAwesome name="times" />
+                </Button>
+              </>
+            )}
+          </Store.Consumer>
+
         </Fragment>
       </FlexItem>
     </Flex>
